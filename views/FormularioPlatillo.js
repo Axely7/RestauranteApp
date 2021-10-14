308aafce1189f69710a726f3a45d04cdfbd8331f
Axel Jimenez <axely7@hotmail.com>
2021-10-13 22:17:09 -0500

    Adding buttons to formularioPlatillo

--- views/FormularioPlatillo.js
+++ views/FormularioPlatillo.js
@@ -1,18 +1,46 @@
 import React from 'react';
-import { NativeBaseProvider, Box, Heading, Image, Stack, Text, HStack, Button, Center, FormControl } from 'native-base';
+import { NativeBaseProvider, Box, Heading, Image, Stack, Text, HStack, Button, Center, FormControl, AddIcon, Input} from 'native-base';
 import PedidoContext from '../context/pedidos/pedidosContext';
 import globalStyles from '../styles/global';
+import {Ionicons} from "@expo/vector-icons"
 import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome5'
 
 const FormularioPlatillo = () => {
     return ( 
         <NativeBaseProvider>
-            <Box>
-                <FormControl>
+            <Stack space={4} alignItems="center">
                     <Heading style={globalStyles.titulo}>Cantidad</Heading>
-                    
-                </FormControl>
-            </Box>
+                    <HStack space={12} alignItems="center">
+                        <Button
+                            props
+                            h="100px"
+                            w="36%"
+                            style={{backgroundColor: '#000'}}
+                        >
+                            <Icon style={{fontSize: 40}} name="minus"></Icon>
+                        </Button>
+                        
+                        <Button
+                            props
+                            dark
+                            style={{backgroundColor: 'transparent'}}
+                        >
+                            <Input 
+                                style={{textAlign: 'center', fontSize: 20}}
+                                value="1"></Input>
+                        </Button>
+
+                        <Button
+                            props
+                            h="100px"
+                            w="36%"
+                            style={{backgroundColor: '#000'}}
+                        >
+                            <Icon style={{fontSize: 40}} name="plus"></Icon>
+                        </Button>
+                    </HStack>
+            </Stack>
         </NativeBaseProvider>
      );
 }
