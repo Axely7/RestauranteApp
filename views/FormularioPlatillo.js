114b47b29116bde022a08bfd6dc761f1c85ea5ed
Axel Jimenez <axely7@hotmail.com>
2021-10-13 21:22:33 -0500

    installing native icon

--- views/FormularioPlatillo.js
+++ views/FormularioPlatillo.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { NativeBaseProvider, Box, Heading, Image, Stack, Text, HStack, Button, Center, FormControl } from 'native-base';
+import PedidoContext from '../context/pedidos/pedidosContext';
+import globalStyles from '../styles/global';
+import { useNavigation } from '@react-navigation/native';
 
 const FormularioPlatillo = () => {
     return ( 
-        <Text>FormularioPlatillo</Text>
+        <NativeBaseProvider>
+            <Box>
+                <FormControl>
+                    <Heading style={globalStyles.titulo}>Cantidad</Heading>
+                    
+                </FormControl>
+            </Box>
+        </NativeBaseProvider>
      );
 }
  
